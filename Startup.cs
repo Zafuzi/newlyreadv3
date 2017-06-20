f34a97b65a1cd7ebd1cf65e5f379f62fba24f559
Zafuzi <zacharyfoutz@gmail.com>
2017-06-20 16:55:20 -0700

    fix: Not sure

--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace NewlyReadv3
             if (env.IsDevelopment())
             {
                 // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
+                //builder.AddUserSecrets();
             }
 
             builder.AddEnvironmentVariables();
